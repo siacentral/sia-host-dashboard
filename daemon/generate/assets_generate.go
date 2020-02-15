769147ac9e36f74a47bbc9ee94fa44b70a17251d
Nate Maninger <me@n8m.us>
2020-02-15 11:11:55 +0900

    fix missing dir issue

--- daemon/generate/assets_generate.go
+++ daemon/generate/assets_generate.go
@@ -10,6 +10,10 @@ import (
 )
 
 func main() {
+	if err := os.MkdirAll("assets/assets.go", 0770); err != nil && !os.IsExist(err) {
+		log.Fatalln(err)
+	}
+
 	err := vfsgen.Generate(http.Dir(os.Args[1]), vfsgen.Options{
 		Filename:     "assets/assets.go",
 		PackageName:  "assets",

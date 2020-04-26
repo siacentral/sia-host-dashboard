7610e73283df8ee916a8813f224cf64689516d61
Nate Maninger <me@n8m.us>
2020-04-26 11:04:58 +0900

    simplify endpoints

--- daemon/web/endpoints.go
+++ daemon/web/endpoints.go
@@ -3,21 +3,21 @@ package web
 import "github.com/siacentral/host-dashboard/daemon/web/router"
 
 var endpoints = []router.APIEndpoint{
-	router.APIEndpoint{
+	{
 		Name:    "Get Snapshots",
 		Method:  "GET",
 		Pattern: "/snapshots",
 		Secure:  false,
 		Handler: handleGetHostSnapshots,
 	},
-	router.APIEndpoint{
+	{
 		Name:    "Get Totals",
 		Method:  "GET",
 		Pattern: "/totals",
 		Secure:  false,
 		Handler: handleGetHostTotals,
 	},
-	router.APIEndpoint{
+	{
 		Name:    "Get Status",
 		Method:  "GET",
 		Pattern: "/status",
